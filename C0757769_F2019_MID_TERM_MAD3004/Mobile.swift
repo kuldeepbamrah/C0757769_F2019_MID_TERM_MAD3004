8e89aa4cbdfbd6976b87067a57da353e40fe60ed
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-10-19 18:01:47 -0400

    14 commit

---
+++ C0757769_F2019_MID_TERM_MAD3004/Mobile.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile.swift
+//  C0757769_F2019_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-10-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+class Mobile {
+    var manufacturerName:String
+    var planName:String
+    var mobileNumber:String
+    var internetUsed:Float
+    var minutesUsed:Int
+    
+}
